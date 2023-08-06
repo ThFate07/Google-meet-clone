8e56c6ad14d6cc80f1ce354ee98c33728b5a61b4
ThFate <kunaalrw2004@gmail.com>
2023-08-07 02:12:59 +0530

    added mongoose server

--- Backend/db/connectDB.js
+++ Backend/db/connectDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 function connectDB() { 
     try { 
-        mongoose.connect('mongodb://127.0.0.1:27017/googlemeet')
+        mongoose.connect('mongodb+srv://thfate:7715821250@cluster0.y4ypebz.mongodb.net/googlemeet')
     } catch  { 
         console.log("couldn't connect to database. please try again later")
     }
